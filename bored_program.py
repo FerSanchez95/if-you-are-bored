4c334aa710780a64fd88587627a19db883443abf
FerSanchez95 <fersanchezz95@gmail.com>
2024-01-28 15:18:36 -0300

    Added the request method and some interface fixes

--- bored_program.py
+++ bored_program.py
@@ -1,29 +1,48 @@
-''' If you are bored you can use this program to found someting to do. https://www.boredapi.com/api/activity'''
-from tkinter import *
-from tkinter import ttk
-import json
+''' 
+    If you are bored you can use this program to found someting to do.
+    API: https://www.boredapi.com/api/activity
+'''
+from tkinter import Tk, Label, Button
+from tkinter import messagebox
 import requests
 
-def do_request():
-    '''Do the reques when the button is pressed.'''
+class DoSometing():
+    ''' To complete this docstring'''
+    def __init__(self):
+        self.win = Tk()
+
+    def do_request(self):
+        '''Do the reques when the button is pressed.'''
+        response = requests.get('https://www.boredapi.com/api/activity', timeout=30)
+        decoded_response = response.json()
+        pop_up_title = decoded_response['type']
+        pop_up_message = decoded_response['activity']
+        messagebox.showinfo(pop_up_title, pop_up_message)
+
+    def interface(self):
+        ''' X '''
+        self.win.title("bored stuff")
+        width = 250
+        height = 60
+        # The next lines are used to center the window.
+        width_scr = self.win.winfo_screenwidth()
+        height_scr = self.win.winfo_screenheight()
+        x = int((width_scr/2) - (width/2))
+        y = int((height_scr/2) - (height/2))
+        self.win.geometry(f"{width}x{height}+{x}+{y}")
+        # Window content.
+        main_message = Label(self.win, text="Press to found someting to do!", font=("bold", 10))
+        main_message.pack(pady=3)
+        btn = Button(self.win, text="Search", command=self.do_request)
+        btn.pack(pady=2)
+        self.win.mainloop()
+
+    def __call__(self):
+        self.interface()
 
 def main():
-    ''' X '''
-    win = Tk()
-    win.title("bored stuff")
-    width = 300
-    height = 200
-    width_scr = win.winfo_screenwidth()
-    height_scr = win.winfo_screenheight()
-    x = (width_scr/2) - (width/2)
-    y = (height_scr/2) - (height/2)
-    win.geometry("%dx%d+%d+%d" % (width, height, x, y))
-
-    sub_title = Label(win, text="Press to found somting to do", font=("bold", 10))
-    sub_title.pack()
-    btn = Button(win, text="botón")
-    btn.pack()
-    win.mainloop()
-
+    '''Execute the class to do someting.'''
+    bored = DoSometing()
+    bored()
 if __name__ == '__main__':
     main()
