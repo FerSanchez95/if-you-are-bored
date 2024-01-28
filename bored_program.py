21cad58fef52c46326096b6bd930ad2b86375e79
FerSanchez95 <fersanchezz95@gmail.com>
2024-01-28 15:26:46 -0300

    added docstring, timeout on request and change main  window title.

--- bored_program.py
+++ bored_program.py
@@ -2,17 +2,17 @@
     If you are bored you can use this program to found someting to do.
     API: https://www.boredapi.com/api/activity
 '''
-from tkinter import Tk, Label, Button
-from tkinter import messagebox
+from tkinter import Tk, Label, Button, messagebox
 import requests
 
 class DoSometing():
-    ''' To complete this docstring'''
+    ''' This class include the interface of the program and the API request handle.'''
     def __init__(self):
+        ''' Initailaize the tkinter instance.'''
         self.win = Tk()
 
     def do_request(self):
-        '''Do the reques when the button is pressed.'''
+        '''Do the request when the button is pressed and print it on a message box.'''
         response = requests.get('https://www.boredapi.com/api/activity', timeout=30)
         decoded_response = response.json()
         pop_up_title = decoded_response['type']
@@ -20,16 +20,18 @@ class DoSometing():
         messagebox.showinfo(pop_up_title, pop_up_message)
 
     def interface(self):
-        ''' X '''
-        self.win.title("bored stuff")
-        width = 250
+        ''' This function defines the main interface of the program.'''
+        self.win.title("Want to do something?")
+        width = 300
         height = 60
+        
         # The next lines are used to center the window.
         width_scr = self.win.winfo_screenwidth()
         height_scr = self.win.winfo_screenheight()
         x = int((width_scr/2) - (width/2))
         y = int((height_scr/2) - (height/2))
         self.win.geometry(f"{width}x{height}+{x}+{y}")
+        
         # Window content.
         main_message = Label(self.win, text="Press to found someting to do!", font=("bold", 10))
         main_message.pack(pady=3)
@@ -44,5 +46,6 @@ def main():
     '''Execute the class to do someting.'''
     bored = DoSometing()
     bored()
+
 if __name__ == '__main__':
     main()
